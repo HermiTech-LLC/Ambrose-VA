76a30ce65dc5d806bef34636eeada036cb893b85
Ant <130005196+LoQiseaking69@users.noreply.github.com>
2024-05-08 15:37:06 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .assistant import query_wolfram_alpha, detect_intent_dialogflow, asynchronous_speak, main_loop, main
+from .assistant import LearningAssistant
 
-__all__ = ["query_wolfram_alpha", "detect_intent_dialogflow", "asynchronous_speak", "main_loop", "main"]
+__all__ = ["LearningAssistant"]
